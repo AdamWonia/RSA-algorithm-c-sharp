bbad70cc1e513009585057eb3e575abc24333261
AdamWonia <adam.wonia@gmail.com>
2022-08-12 20:37:46 +0200

    Added function for calculating greatest common divisor

--- RSA/Program.cs
+++ RSA/Program.cs
@@ -9,4 +9,21 @@ namespace RSA
             Console.WriteLine("Hello World!");
         }
     }
+
+    public class RSA
+    {
+        public int GreatestCommonDivisor(int a, int b)
+        {
+            while (a != b)
+            {
+                if (a > b)
+                    a = a - b;
+                else
+                    b = b - a;
+            }
+            return a;
+        }
+
+
+    }
 }
